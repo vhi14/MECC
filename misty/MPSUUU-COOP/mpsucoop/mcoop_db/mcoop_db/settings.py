ddb0b4afaee5965a43d881ba1c336905d14261f3
Axen14 <glitzlhen124@gmail.com>
2025-12-11 18:34:09 +0800

    removed hardcoded settings in settings.py

--- misty/MPSUUU-COOP/mpsucoop/mcoop_db/mcoop_db/settings.py
+++ misty/MPSUUU-COOP/mpsucoop/mcoop_db/mcoop_db/settings.py
@@ -166,12 +166,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS =  [
-    'http://localhost:3000',
-]
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-]
+# CORS_ALLOWED_ORIGINS =  [
+#     'http://localhost:3000',
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
 CORS_ALLOW_METHODS = [
     'GET',
     'POST',

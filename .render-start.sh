1a9c75006e1bb56fbfd78c6b9766b095b63329b5
vhi14 <vhi299992@gmail.com>
2026-01-29 10:22:17 +0800

    hmmm

---
+++ .render-start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Starting Django application..."
+cd misty/MPSUUU-COOP/mpsucoop/mcoop_db
+python manage.py migrate --noinput
+echo "Running gunicorn..."
+gunicorn mcoop_db.wsgi:application --bind 0.0.0.0:$PORT --log-level debug --access-logfile - --error-logfile -

472ca10a50dbc6c53cf0f546713acd2796ea13d8
vhi14 <vhi299992@gmail.com>
2026-01-29 10:04:01 +0800

    hanother

---
+++ .render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Python dependencies..."
+python -m pip install --upgrade pip
+pip install -r misty/MPSUUU-COOP/mpsucoop/mcoop_db/mcoop_db/requirements.txt
+
+echo "Collecting static files..."
+cd misty/MPSUUU-COOP/mpsucoop/mcoop_db
+python manage.py collectstatic --noinput

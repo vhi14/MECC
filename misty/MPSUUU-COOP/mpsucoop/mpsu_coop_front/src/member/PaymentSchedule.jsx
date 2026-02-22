5b867cbdd406848a4262c78fd96e648e01ac5102
unknown <bernardezmarbert972@gmail.com>
2026-02-22 21:15:00 +0800

    final

--- misty/MPSUUU-COOP/mpsucoop/mpsu_coop_front/src/member/PaymentSchedule.jsx
+++ misty/MPSUUU-COOP/mpsucoop/mpsu_coop_front/src/member/PaymentSchedule.jsx
@@ -221,7 +221,7 @@ const PaymentSchedule = () => {
               width: 100% !important;
             }
             .table-scroll-container {
-              max-height:600px !important;
+              max-height:800px !important;
             }
             .payment-table {
               font-size: 11px !important;
